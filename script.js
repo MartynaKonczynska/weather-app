ff492111e953033036ff76ff15ad2c55fbf75ab6
MartynaKonczynska <markonczynska@gmail.com>
2021-09-26 13:36:36 +0200

    Added weather icon change and updated background

--- script.js
+++ script.js
@@ -55,6 +55,12 @@ function showTemperature(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+
+  let iconElement = document.querySelector("#weather-icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 function locateUser(position) {
